1c97595bdfef4ba0a6b1065bce418b3b0b715c63
Rares Colf <rares.colf88@gmail.com>
2022-11-16 18:17:37 +0200

    BMS-15: Add Hibernate configuration(pom and utils).

---
+++ src/main/java/com/sda/rares/bookmanagement/utils/SessionManager.java
@@ -0,0 +1,37 @@
+package com.sda.rares.bookmanagement.utils;
+
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Trebuie sa ne asiguram ca aceasta clasa are un pattern de Singleton
+ *     - Constructorul in patternul Sigleton trebuie sa fie private ca sa putem controla de cate ori il instantiem
+ *     - Instantiem " private static final SessionManager INSTANCE  = new SessionManager(); " si o facem constanta si o facem sa fie una singura prin Static,
+ *        si vrem sa fie si final ca sa nu o putem modifica
+ *      - cel mai simplu mod de a face o constanta.
+ *      CRUD = create, read, update, delete.
+ */
+
+public class SessionManager extends AbstractSessionManager {
+
+    private static final SessionManager INSTANCE  = new SessionManager();
+
+    private SessionManager(){
+    }
+
+    public static SessionFactory getSessionFactory(){
+        return INSTANCE.getSessionFactory("book_management_system");   
+    }
+
+    public static void shutDown(){
+        INSTANCE.shutdownSessionManager();
+    }
+
+
+    @Override
+    protected void setAnnotatedClasses(Configuration configuration) {
+        //Hibernate model will be added here
+
+    }
+}
